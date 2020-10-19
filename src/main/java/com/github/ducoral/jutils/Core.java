2957e22c3a80e229f55999248a06542ca51b6eee
ducoral <ducoral@gmail.com>
2020-10-18 22:07:05 -0300

    Implementação de params(String, List<String>) e prepare(PreparedStatement, List<String>, Map<String, Object>)

--- src/main/java/com/github/ducoral/jutils/Core.java
+++ src/main/java/com/github/ducoral/jutils/Core.java
@@ -1,12 +1,17 @@
 package com.github.ducoral.jutils;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URL;
+import java.sql.*;
 import java.util.*;
+import java.util.function.BooleanSupplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Core {
 
-    public static final Pattern PARAM_PATTERN = Pattern.compile(paramRegex("((\\w+\\.)?\\w+)"));
+    public static final Pattern PARAM_PATTERN = Pattern.compile(String.format("\\$\\{%s}", "((\\w+\\.)?\\w+)"));
 
     public enum Align { LEFT, CENTER, RIGHT}
 
@@ -86,13 +91,64 @@ public final class Core {
         while (matcher.find()) {
             String param = matcher.group(1);
             params.add(param);
-            string = string.replaceFirst(paramRegex(param), "?");
+            string = string.replaceFirst(String.format("\\$\\{%s}", param), "?");
         }
         return string;
     }
 
-    private static String paramRegex(String fieldRegex) {
-        return String.format("\\$\\{%s}", fieldRegex);
+    public static PreparedStatement prepare(PreparedStatement st, List<String> params, Map<String, Object> scope) {
+        try {
+            for (int index = 0; index < params.size(); index++) {
+                Object value = scope.get(params.get(index));
+                if (value == null)
+                    st.setNull(index, Types.NULL);
+                else if (value instanceof Byte)
+                    st.setByte(index, (Byte) value);
+                else if (value instanceof Short)
+                    st.setShort(index, (Short) value);
+                else if (value instanceof Integer)
+                    st.setInt(index, (Integer) value);
+                else if (value instanceof Long)
+                    st.setLong(index, (Long) value);
+                else if (value instanceof Float)
+                    st.setFloat(index, (Float) value);
+                else if (value instanceof Double)
+                    st.setDouble(index, (Double) value);
+                else if (value instanceof BigDecimal)
+                    st.setBigDecimal(index, (BigDecimal) value);
+                else if (value instanceof String)
+                    st.setString(index, (String) value);
+                else if (value instanceof java.sql.Date)
+                    st.setDate(index, (java.sql.Date) value);
+                else if (value instanceof java.sql.Time)
+                    st.setTime(index, (java.sql.Time) value);
+                else if (value instanceof java.sql.Timestamp)
+                    st.setTimestamp(index, (java.sql.Timestamp) value);
+                else if (value instanceof Boolean)
+                    st.setBoolean(index, (Boolean) value);
+                else if (value instanceof byte[])
+                    st.setBytes(index, (byte[]) value);
+                else if (value instanceof Ref)
+                    st.setRef(index, ((Ref) value));
+                else if (value instanceof NClob)
+                    st.setNClob(index, (NClob) value);
+                else if (value instanceof Clob)
+                    st.setClob(index, (Clob) value);
+                else if (value instanceof Blob)
+                    st.setBlob(index, (Blob) value);
+                else if (value instanceof Array)
+                    st.setArray(index, (Array) value);
+                else if (value instanceof URL)
+                    st.setURL(index, (URL) value);
+                else if (value instanceof SQLXML)
+                    st.setSQLXML(index, (SQLXML) value);
+                else
+                    st.setObject(index, value);
+            }
+        } catch (Exception e) {
+            throw new Oops(e.getMessage(), e);
+        }
+        return st;
     }
 
     private Core() {
